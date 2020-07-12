7b0cd9ee825aaf423de20e3e9273b9d35e46ed63
Abhisek Yadav <abhisek.yadav@sap.com>
2020-07-12 16:48:22 +0530

    docker-compose example

--- spring-dockercompose-example/src/main/java/com/test/controller/VisitCountController.java
+++ spring-dockercompose-example/src/main/java/com/test/controller/VisitCountController.java
@@ -2,14 +2,28 @@ package com.test.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.Jedis;
 
 @RestController
 public class VisitCountController {
 
-  @GetMapping("/ping")
-  public String ping() {
-    System.err.println("ping call");
-    return "Hello World!";
-  }
+    @GetMapping("/ping")
+    public String ping() {
+        System.err.println("ping call");
+        return "Hello World!";
+    }
+
+    @GetMapping("/visits")
+    public String visitCount() {
+        Jedis jedis = new Jedis("redis");
+
+        if (jedis.get("visit") == null)
+            jedis.set("visit", String.valueOf(0));
+
+        int visits = Integer.parseInt(jedis.get("visit"));
+        jedis.set("visit", String.valueOf(visits + 1));
+
+        return String.valueOf(visits);
+    }
 
 }
